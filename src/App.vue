6fd7497ac5a5ba62c4564c23d7b90acc7816a1d2
zhonghanlu <1420865757@qq.com>
2025-02-27 23:00:31 +0800

    DESC:[init]

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Three from "./components/Three.vue";
+</script>
+
+<template>
+  <Three msg="Test"/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Three.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    {{ msg}}
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
+
+<style scoped>
+</style>
