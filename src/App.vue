3ea14fb378fe028e5cf7250959caf5d70be0a8e9
zhonghanlu <1420865757@qq.com>
2025-02-27 23:12:02 +0800

    DESC:[init]

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Three from "./components/Three.vue";
 </script>
 
+<!--https://www.tripo3d.ai/app/model/4edef747-09cf-4d65-9992-0aea0e280e42-->
 <template>
   <Three msg="Test"/>
 </template>
